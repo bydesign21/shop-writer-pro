548cb38942c3591a03c6565eca04569edc9b124f
ByDesign21 <105322762+bydesign21@users.noreply.github.com>
2023-05-21 23:38:25 -0500

    Revert "Upgrade to Angular 16"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,19 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools'
+import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { SessionService } from './session-store/domain-state/session.service';
 import { SpinnerModule } from 'src/features/shared-module/spinner/spinner.module';
 import { HttpClientModule } from '@angular/common/http';
 import { TicketStore } from 'src/features/dashboard-module/ticketing/store/tickets.store';
-import { IconDefinition } from '@ant-design/icons-angular';
-import * as AllIcons from '@ant-design/icons-angular/icons';
 
 /** config angular i18n **/
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { HomepageComponent } from 'src/features/corp-site/homepage.component';
 registerLocaleData(en);
-
-// configure app Icons Statically :/
-const antDesignIcons = AllIcons as {
-  [key: string]: IconDefinition;
-};
-const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
 
 @NgModule({
   declarations: [
@@ -34,11 +27,14 @@ const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesign
     BrowserModule,
     AuthModule,
     AppRoutingModule,
+    GooglePlaceModule,
     AkitaNgDevtools.forRoot({}),
     SpinnerModule,
-    NzIconModule.forRoot(icons),
     HttpClientModule,
     BrowserAnimationsModule
+  ],
+  entryComponents: [
+    HomepageComponent
   ],
   providers: [
     SessionService,
--- src/features/auth-module/auth.guard.ts
+++ src/features/auth-module/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Router, UrlTree } from '@angular/router';
+import { CanActivate, Router, UrlTree } from '@angular/router';
 import { map, Observable, take } from 'rxjs';
 import { AuthService } from './auth-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard  {
+export class AuthGuard implements CanActivate {
   constructor(
     private authService: AuthService,
     private router: Router

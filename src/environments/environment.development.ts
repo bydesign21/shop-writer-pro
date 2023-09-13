93e3524787b696bc0cc8010d70f92ebadd30b542
Logan Vasquez <loganvasquez@logans-mbp.lan>
2023-09-12 19:55:40 -0500

    Updated stripe api env variable to use live mode stripe pk

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  STRIPE_API: 'pk_test_51MVpHRIQ2JRXZFlUPWOlmMdpAQBZy2ShjwPFt5LOdX9T2nX25EvEWP0VJD0HJC2LZjMnQJKWo0ogJCCoubhha3F800EWRPDsmL',
+  STRIPE_API: 'pk_live_51NpaEfL21evYAUlPAB6QA8ChVu9PbtAbaRrsNJ8VUo6ogVYvuTWLzQjuiG68gVn6QQfXlSskJ3dcMhTvVONTIjXd00gNvMFIkE',
   GOOGLE_MAPS_API: 'AIzaSyABcNJOiFPqFdDvQFJinlQ4NgyMZvzoRig'
 };

b642c71190750af0320dddd205a0fdddd75b6068
Logan Vasquez <loganvasquez@logans-mbp.lan>
2023-09-26 19:31:19 -0500

    Staging awsexports

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -6,14 +6,14 @@ const awsmobile = {
   "aws_cloud_logic_custom": [
       {
           "name": "coreAPI",
-          "endpoint": "https://8hbtkzkq1a.execute-api.us-east-1.amazonaws.com/prod",
+          "endpoint": "https://mcbwuxn2ri.execute-api.us-east-1.amazonaws.com/staging",
           "region": "us-east-1"
       }
   ],
-  "aws_cognito_identity_pool_id": "us-east-1:e53e6246-1bc1-4977-88bc-5e2dfe8f6c48",
+  "aws_cognito_identity_pool_id": "us-east-1:78eba615-820c-4b9b-ab65-24e2e9734127",
   "aws_cognito_region": "us-east-1",
-  "aws_user_pools_id": "us-east-1_lACvNJfGF",
-  "aws_user_pools_web_client_id": "fg52uibn8fdivffg8bo1j562n",
+  "aws_user_pools_id": "us-east-1_9y4DrDyvq",
+  "aws_user_pools_web_client_id": "4h05tm03ocdfrfduk3mc8a4bq4",
   "oauth": {},
   "aws_cognito_username_attributes": [
       "EMAIL"
@@ -39,11 +39,11 @@ const awsmobile = {
   "aws_dynamodb_all_tables_region": "us-east-1",
   "aws_dynamodb_table_schemas": [
       {
-          "tableName": "coreDynamo-prod",
+          "tableName": "coreDynamo-staging",
           "region": "us-east-1"
       }
   ],
-  "aws_user_files_s3_bucket": "shopwriterpros3191212-prod",
+  "aws_user_files_s3_bucket": "shopwriterpros3204800-staging",
   "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
